f335292e35150906958646598433e667373e8642
tsong0ku <sonbappe89@gmail.com>
2023-11-17 17:11:44 +0700

    base layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,100 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from '@mui/material/Button';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import Box from '@mui/material/Box';
+
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme,
+} from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import DarkModeOutlinedICon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import LightModeIcon from '@mui/icons-material/LightMode'
+import Container from '@mui/material/Container';
+ function ModeSelect() {
+  const {mode,setMode} = useColorScheme();
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode)
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        label="Mode"
+        onChange={handleChange}
+        value={mode}
+      >
+      
+        <MenuItem value='light'>
+          <div style={{display:'flex', alignItems: 'center',gap:'8px'}}>
+          <LightModeIcon/>
+          Light
+          </div>
+          </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{display:'flex',alignItems: 'center',gap:1}}>
+      <DarkModeOutlinedICon/>
+      
+          Dark
+          </Box>
+          </MenuItem>
+        <MenuItem value='system'>
+        <Box sx={{display:'flex',alignItems: 'center',gap:1}}>
+
+        <SettingsBrightnessIcon/>
+          System
+      </Box>
+          
+          </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
+
+
 
 function App() {
 
+
   return (
     <>
-      <Button variant="contained">Hello world</Button>
-      <AccessAlarmIcon></AccessAlarmIcon>
+    <Container disableGutters maxWidth={false}  sx={{height: '100vh'}}>
+    <Box sx={{
+      backgroundColor: 'primary.light',
+      width: '100%',
+      height: (theme) => theme.trello.appBarHeight,
+      display: 'flex',
+      alignItems: 'center'
+    }}>
+      <ModeSelect></ModeSelect>
+    </Box>
+    <Box sx={{
+      backgroundColor: 'primary.dark',
+      width: '100%',
+      height: (theme) => theme.trello.boardBarHeight,
+
+      display: 'flex',
+      alignItems: 'center'
+    }}>Board Bar</Box>
+    <Box sx={{
+      backgroundColor: 'primary.main',
+      width: '100%',
+      height:(theme) =>  `calc(100vh - ${ theme.trello.appBarHeight } - ${ theme.trello.boardBarHeight })}`,
+      display: 'flex',
+      alignItems: 'center'
+    }}>Content</Box>
+    </Container>
+      
     </>
   )
 }
---
+++ src/theme.js
@@ -0,0 +1,30 @@
+import { deepOrange, orange, red, teal } from '@mui/material/colors';
+import { experimental_extendTheme as extendTheme} from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = extendTheme({
+  trello:{
+    appBarHeight: '48px',
+    boardBarHeight: '58px',
+
+  },
+    colorSchemes: {
+      light: {
+        palette: {
+          primary: teal,
+            secondary: deepOrange
+          },
+        },
+      },
+      dark: {
+        palette: {
+          primary:  red,
+            secondary: orange
+
+          },
+        },
+      },
+    
+    // ...other properties
+  );
+export default theme;
